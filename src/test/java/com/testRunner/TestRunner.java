663eb1c417d86ea7d39be79e27a5cd57f12e6e79
Dhanalakshmi <ksureka924@gamil.com>
2024-10-21 17:09:43 +0530

    updated jenkins

--- src/test/java/com/testRunner/TestRunner.java
+++ src/test/java/com/testRunner/TestRunner.java
@@ -9,11 +9,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(glue = "com.step",features = "src\\test\\resources\\Feature",dryRun=false,plugin= {"json:target\\jenkins.json"})
+@CucumberOptions(glue = "com.step",features = "src/test/resources/Feature",dryRun=false,plugin= {"json:target/jenkins.json"})
 public class TestRunner {
 	@AfterClass
 	public static void afterExecution() {
-		Reporting.generateJVMReport(System.getProperty("user.dir") + "\\target\\jenkins.json");
+		Reporting.generateJVMReport(System.getProperty("user.dir") + "/target/jenkins.json");
 	}
 
 }
